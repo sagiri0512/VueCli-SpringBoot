872131b7df9062aade196f70ebc111258e976529
sagiri <sagiri0512@qq.com>
2024-05-30 16:29:23 +0800

    v 1.0.1 发送邮箱验证码，验证验证码是否正确

---
+++ src/main/java/com/sagiri/DemoHelloApplication.java
@@ -0,0 +1,15 @@
+package com.sagiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class DemoHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHelloApplication.class, args);
+    }
+
+}
